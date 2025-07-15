f4f304cf82e10b88cc9ecc6aa094192357931666
ludoguenet <ludoguenet@gmx.com>
2025-07-15 22:53:40 +0200

    fix: user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { VideoController } from './video/video.controller';
 import { VideoService } from './video/video.service';
 import { VideoModule } from './video/video.module';
 import { UploadModule } from './upload/upload.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [VideoModule, UploadModule],
+  imports: [
+    VideoModule,
+    UploadModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'bass_buddy',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController, VideoController],
   providers: [AppService, VideoService],
 })
